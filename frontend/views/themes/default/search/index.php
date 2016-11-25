d024c79126912a0cd9ec9ab8616e65e689d08c1e
“caozongchao” <caozongchao@163.com>
2016-11-25 02:16:57 +0000

    ..

--- frontend/views/themes/default/search/index.php
+++ frontend/views/themes/default/search/index.php
@@ -2,15 +2,16 @@
 use yii\widgets\ListView;
 use yii\helpers\Url;
 use yii\widgets\LinkPager;
+use common\models\View;
 /* @var $this yii\web\View */
 
-$this->params['breadcrumbs'][] = $view['name'];
 ?>
 <div id="fh5co-main">
     <div class="fh5co-gallery">
         <?php foreach ($views as $key => $view): ?>
             <a class="gallery-item" href="<?=Url::to(['site/show','id' => $view['id']],true);?>">
-                <img src="<?=Yii::$app->params['hostUrl'].'/'.$view['scan_img']?>" alt="<?=$view['name']?>">
+            <?php $viewAll = View::findOne($view['id']);?>
+                <img src="<?=Yii::$app->params['hostUrl'].'/'.$viewAll['scan_img']?>" alt="<?=$view['name']?>">
                 <span class="overlay">
                     <h2><?=$view['name']?></h2>
                 </span>
