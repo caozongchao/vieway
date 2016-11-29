9fbac70035368889ca7e278f0413c8533b3ac4b7
“caozongchao” <caozongchao@163.com>
2016-11-29 02:00:01 +0000

    ..

--- frontend/controllers/SearchController.php
+++ frontend/controllers/SearchController.php
@@ -8,12 +8,15 @@ use yii\data\Pagination;
 class SearchController extends Controller{
 
     public function actionIndex(){
+        $key = Yii::$app->request->get('key');
         $db = Yii::$app->xunsearch->getDatabase('view');
         // $db = Yii::$app->xunsearch('duanzi');
         $xs = $db->xs;
+        // $tokenizer = $db->scws;
+        // $words = $tokenizer->getResult($key);
+        // var_dump($words);die;
         $search = $db->getSearch();
         $index = $db->getIndex();
-        $key = Yii::$app->request->get('key');
         // $results = $search->search($key);
         $pages = new Pagination(['totalCount'=>$search->count($key)]);
         $results = $search->setQuery($key)->setLimit($pages->limit,$pages->offset)->search();
