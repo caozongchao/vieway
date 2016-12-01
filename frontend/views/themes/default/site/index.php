29b928e741d8fd9a9f5fb4787c5456f33aa67bd5
caozongchao <caozongchao@163.com>
2016-12-01 14:53:47 +0800

    ..

--- frontend/views/themes/default/site/index.php
+++ frontend/views/themes/default/site/index.php
@@ -103,7 +103,7 @@ function ajaxCities()
         $("#selectC").append("<dd><a href=\"javascript:void(0)\" id=\"2459\">重庆</a></dd>");
     }else{
         $.ajax({
-            url: '<?=Url::to(['site/ajax-get-views'])?>',
+            url: '<?=Url::to(['addi/ajax-get-citys'])?>',
             type: 'GET',
             dataType: 'json',
             data: {id: id},
