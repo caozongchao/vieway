5df3a8dc9c5f9865411763974b4b80cfb8863113
caozongchao <caozongchao@163.com>
2016-12-12 10:21:08 +0800

    .

--- common/config/params.php
+++ common/config/params.php
@@ -5,5 +5,5 @@ return [
     'user.passwordResetTokenExpire' => 3600,
     'hostUrl' => 'http://www.vieway.cn/',
     'hostAdmin' => 'http://www.caozongchao.cn/',
-    'qnUrl' => 'http://7xsinm.com1.z0.glb.clouddn.com',
+    'qnUrl' => 'http://7xsinm.com1.z0.glb.clouddn.com/',
 ];
